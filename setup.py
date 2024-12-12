88c960239310f6cf0ef300bb40785ad24e5d2c49
Filip Michalsky <filipmichalsky@gmail.com>
2024-12-12 08:18:25 -0500

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="stagehand-sdk",
+    name="stagehand-py",
     version="0.1.0",
     author="BrowserBase",
     author_email="support@browserbase.io",
